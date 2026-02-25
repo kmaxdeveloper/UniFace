2e1a03825f32a06490c91b6d7dfca250d1f34c9f
kmaxdeveloper <komilcoder7@gmail.com>
2026-02-25 20:03:02 +0500

    UniFace - Fix bugs 5

--- src/main/kotlin/controller/StudentController.kt
+++ src/main/kotlin/controller/StudentController.kt
@@ -8,18 +8,25 @@ import org.springframework.web.multipart.MultipartFile
 @RequestMapping("/api/uniface")
 class StudentController(private val faceService: FaceService) {
 
-//    @PostMapping("/register")
-//    fun register(@RequestParam("id") id: String, @RequestParam("image") file: MultipartFile): String {
-//        return faceService.registerFace(id, file.bytes)
-//    }
-//
-//    @GetMapping("/setup")
-//    fun setup(): String {
-//        return faceService.createCollection()
-//    }
-//
-//    @PostMapping("/identify") // Aynan POST ekanligini tekshir
-//    fun identify(@RequestParam("image") file: MultipartFile): String {
-//        return faceService.identifyStudent(file.bytes)
-//    }
+    @PostMapping("/register")
+    fun register(
+        @RequestParam("id") id: String,
+        @RequestParam("fullName") fullName: String,    // Buni qo'shdik
+        @RequestParam("groupName") groupName: String,  // Buni qo'shdik
+        @RequestParam("image") file: MultipartFile
+    ): Any {
+        // FaceService.registerFace(id, fullName, groupName, imageBytes) shaklida bo'lishi kerak
+        return faceService.registerFace(id, fullName, groupName, file.bytes)
+    }
+
+    @GetMapping("/setup")
+    fun setup(): Any {
+        return faceService.createCollection()
+    }
+
+    @PostMapping("/identify")
+    fun identify(@RequestParam("image") file: MultipartFile): Any {
+        // FaceService.identifyStudent(imageBytes)
+        return faceService.identifyStudent(file.bytes)
+    }
 }
