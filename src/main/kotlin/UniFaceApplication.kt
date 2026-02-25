debd3f818afef5b35421ff1fbe4231428bee3274
kmaxdeveloper <komilcoder7@gmail.com>
2026-02-25 19:51:41 +0500

    UniFace - Fix bugs 4

--- src/main/kotlin/UniFaceApplication.kt
+++ src/main/kotlin/UniFaceApplication.kt
@@ -1,12 +1,13 @@
 package com.uniface
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.boot.autoconfigure.domain.EntityScan
 
-@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
+@SpringBootApplication // EXCLUDE QISMINI O'CHIRDIK!
 @EnableJpaRepositories("com.uniface.repository")
+@EntityScan("com.uniface.entity")
 class UniFaceApplication
 
 fun main(args: Array<String>) {
