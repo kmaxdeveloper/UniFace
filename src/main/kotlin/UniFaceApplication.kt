28e4d6fac14a392f4411f584fb437f3a416a8c21
kmaxdeveloper <komilcoder7@gmail.com>
2026-02-25 09:14:26 +0500

    UniFace - Edited

--- src/main/kotlin/UniFaceApplication.kt
+++ src/main/kotlin/UniFaceApplication.kt
@@ -3,8 +3,10 @@ package com.uniface
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
+@EnableJpaRepositories("com.uniface.repository")
 class UniFaceApplication
 
 fun main(args: Array<String>) {
