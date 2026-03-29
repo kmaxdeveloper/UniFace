bba405478d1b0e1e34e0c7d3421b017d24f5f22a
kmaxdeveloper <komilcoder7@gmail.com>
2026-03-29 22:10:27 +0500

    UniFace - Update V53

--- src/main/kotlin/config/SecurityConfig.kt
+++ src/main/kotlin/config/SecurityConfig.kt
@@ -44,7 +44,7 @@ class SecurityConfig(
 
                 // Auth va ochiq endpointlar
                 auth.requestMatchers("/api/v1/auth/**").permitAll()
-                auth.requestMatchers("/api/v1/student/scan/**").permitAll()
+                auth.requestMatchers("/api/v1/student/**").hasAnyAuthority("ROLE_STUDENT", "ROLE_ADMIN")
 
                 // Qolgan rollar bo'yicha cheklovlar
                 auth.requestMatchers("/api/v1/teacher/generate-qr/**").hasAnyAuthority("ROLE_TEACHER", "ROLE_ADMIN")
