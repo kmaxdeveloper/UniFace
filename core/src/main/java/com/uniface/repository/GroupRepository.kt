f1b0b017bc0196fe45ac510310389dd9957e57f8
kmaxdeveloper <komilcoder7@gmail.com>
2026-04-02 00:29:41 +0500

    UniFace - Update V65

--- core/src/main/java/com/uniface/repository/GroupRepository.kt
+++ core/src/main/java/com/uniface/repository/GroupRepository.kt
@@ -11,10 +11,6 @@ interface GroupRepository : JpaRepository<StudentGroup, Long> {
     // 1. Nom bo'yicha qidirish (masalan: "611-21")
     fun findByName(name: String): StudentGroup?
 
-    @Query("SELECT g FROM StudentGroup g WHERE g.faculty.name = :facultyName")
-    fun findAllByFacultyName(facultyName: String): List<StudentGroup>
-
-    // Agar metodni nomi bo'yicha ishlatmoqchi bo'lsang:
     fun findAllByFaculty_Name(name: String): List<StudentGroup>
 
     // 3. O'qituvchiga biriktirilgan guruhlarni ID orqali olish (Custom Query)
