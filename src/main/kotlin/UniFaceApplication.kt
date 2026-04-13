e92eff6cf362de6b86c65931a0c26353d22494a0
kmaxdeveloper <komilcoder7@gmail.com>
2026-04-13 10:31:44 +0500

    UniFace - Update V92

--- src/main/kotlin/UniFaceApplication.kt
+++ src/main/kotlin/UniFaceApplication.kt
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = ["com.uniface"]) // Butun com.uniface paketini skaner qiladi
 @EnableJpaRepositories("com.uniface.repository")
 @EntityScan("com.uniface.entity")
 class UniFaceApplication
