28a83375eed5cae27864e0198c7e47533a747e2d
kmaxdeveloper <komilcoder7@gmail.com>
2026-04-15 11:57:28 +0500

    UniFace - Update V103

--- src/main/kotlin/controller/matrix/MatrixController.kt
+++ src/main/kotlin/controller/matrix/MatrixController.kt
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/matrix")
+@RequestMapping("/api/v1/admin/matrix")
 class MatrixController(
     private val matrixService: MatrixService
 ) {
